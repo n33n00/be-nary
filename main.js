d668fdbe2eaac9683d39396a9fb20ede49ee9188
n33n00 <dizzyninoo@gmail.com>
2020-08-17 19:51:35 +0200

    started js logic

---
+++ main.js
@@ -0,0 +1,21 @@
+
+//console.log("toto");
+
+function decimal2binary() { 
+   // alert("toto");
+    var n = document.getElementById("decimal").value;
+    //document.getElementById("decimal");
+    //alert(n);
+    return false;
+}
+
+function binary2decimal() {
+    var n = document.getElementById("binary").value;
+    //document.getElementById("binary");
+    //console.log(n);
+    return false;
+}
+
+/* Bon voila, Ludo m'a un peu aide.. le console.log des 2 fonctions ne fonctionnent pas.. 
+c'est pour ca que j'ai laisse le alert sur la myFunction1
+Si tu as une idee?? */
