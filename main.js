e9c35c6e97145837c339caa7d0b5fbac192cbc2a
n33n00 <dizzyninoo@gmail.com>
2021-03-01 12:38:46 +0100

    add html/css

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 
 //console.log("toto") // un classique hihi 
+const decInput = document.querySelector("#decimal");
+
 
 function decimal2binary() {
-    var decimal = document.getElementById("decimal").value;
-    return console.log(decimal);
+    //var decimal = document.getElementById("decimal").value;
+    //return console.log(decimal);
 }
 
-function binary2decimal() {
-    var binary = document.getElementById("binary").value;
-    return console.log(binary);
-}
+// function binary2decimal() {
+//     var binary = document.getElementById("binary").value;
+//     return console.log(binary);
+// }
 
 // Affiche le resultat du console.log dans la vue (donc dans index.html) dans notre paragraphe <p></p>
 // Indice 1 => donner un id a chaque paragraphe. 
